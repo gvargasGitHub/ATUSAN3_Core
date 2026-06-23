7e67bbcc656b6a11523cc63d0bc4df39b87bdde0
gvargasGitHub <gvargas@jebla.com>
2026-06-23 09:09:08 -0600

    Upgrade AJAX/Base URL

---
+++ src/Http/URL.php
@@ -0,0 +1,31 @@
+<?php
+namespace Atusan\Http;
+
+class URL
+{
+  // Source - https://stackoverflow.com/a/8891890
+  // Posted by Timo Huovinen, modified by community. See post 'Timeline' for change history
+  // Retrieved 2026-06-22, License - CC BY-SA 4.0
+  static public function origin(array $s, bool $use_forwarded_host = false ) : string
+  {
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+  }
+
+  static public function full( array $s, $use_forwarded_host = false ): string
+  {
+    return self::origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+  }
+
+  static public function base(array $s, $use_forwarded_host=false)
+  {
+		return self::origin($s, $use_forwarded_host)
+      . str_replace("?{$s['QUERY_STRING']}", '', $s['REQUEST_URI']);
+	}
+}
